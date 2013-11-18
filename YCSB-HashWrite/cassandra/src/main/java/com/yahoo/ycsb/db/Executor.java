71131ad5ad43d76384e47acede7e1040f00959e3
pgaref <pangaref@gmail.com>
2013-11-18 22:44:09 +0200

    Create Executor.java

---
+++ YCSB-HashWrite/cassandra/src/main/java/com/yahoo/ycsb/db/Executor.java
@@ -0,0 +1,140 @@
+package com.yahoo.ycsb.db;
+
+/**
+ * A simple example program to use DataMonitor to start and
+ * stop executables based on a znode. The program watches the
+ * specified znode and saves the data that corresponds to the
+ * znode in the filesystem. It also starts the specified program
+ * with the specified arguments when the znode exists and kills
+ * the program if the znode goes away.
+ */
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+public class Executor
+    implements Watcher, Runnable, DataMonitor.DataMonitorListener
+{
+    String znode;
+
+    DataMonitor dm;
+
+    ZooKeeper zk;
+
+    Process child;
+
+    public Executor(String hostPort, String znode) throws KeeperException, IOException {
+        zk = new ZooKeeper(hostPort, 3000, this);
+        dm = new DataMonitor(zk, znode, null, this);
+    }
+
+    /**
+     * @param args
+     
+    public static void main(String[] args) {
+        
+        String hostPort = "109.231.85.43:2181";
+        String znode = "/cazooMaster";
+       // String exec[] = new String[args.length - 3];
+        
+        try {
+            new Executor(hostPort, znode, null).run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /***************************************************************************
+     * We do process any events ourselves, we just need to forward them on.
+     *
+     * @see org.apache.zookeeper.Watcher#process(org.apache.zookeeper.proto.WatcherEvent)
+     */
+    public void process(WatchedEvent event) {
+        dm.process(event);
+    }
+
+    public void run() {
+        try {
+            synchronized (this) {
+                while (!dm.dead) {
+                    wait();
+                }
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public void closing(int rc) {
+        synchronized (this) {
+            notifyAll();
+        }
+    }
+
+    static class StreamWriter extends Thread {
+        OutputStream os;
+
+        InputStream is;
+
+        StreamWriter(InputStream is, OutputStream os) {
+            this.is = is;
+            this.os = os;
+            start();
+        }
+
+        public void run() {
+            byte b[] = new byte[80];
+            int rc;
+            try {
+                while ((rc = is.read(b)) > 0) {
+                    os.write(b, 0, rc);
+                }
+            } catch (IOException e) {
+            }
+
+        }
+    }
+
+    public void exists(byte[] data) {
+        if (data == null) {
+            if (child != null) {
+                System.out.println("Killing process");
+                child.destroy();
+                try {
+                    child.waitFor();
+                } catch (InterruptedException e) {
+                }
+            }
+            child = null;
+        } else {
+            if (child != null) {
+                System.out.println("Stopping child");
+                child.destroy();
+                try {
+                    child.waitFor();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                System.out.println("Starting child - New event");
+                String ringstate = new String(data, "UTF-8");
+                System.out.println("Data: "+  ringstate);
+                
+                String[] binds=ringstate.split("/");
+                String[] tmp=binds[1].split(":");
+                String host=tmp[0];	//ip
+                String port=tmp[1];	//port
+                com.yahoo.ycsb.db.CassandraClient10.pgarefinit(host);
+                //child = Runtime.getRuntime().exec(exec);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
